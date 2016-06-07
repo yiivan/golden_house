8f817f74e2267f89de5895751b837b4793eca151
yiivan <yiivan@hotmail.com>
2016-06-07 10:41:13 -0700

    refactor

--- app/assets/javascripts/my/memos.js
+++ app/assets/javascripts/my/memos.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $(".new-memo-button").on("click", ".show-new-memo-button", function(){
     $(this).toggleClass("show-new-memo-button hide-new-memo-button").text("Hide New Memo");
+    $("html, body").animate({ scrollTop: $(document).height() });
   });
 
   $(".new-memo-button").on("click", ".hide-new-memo-button", function(){
