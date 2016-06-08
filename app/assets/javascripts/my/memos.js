6858286853eb9cb0caed9f1e54b550dca7c57c81
yiivan <yiivan@hotmail.com>
2016-06-07 21:11:31 -0700

    add segment scroll down

--- app/assets/javascripts/my/memos.js
+++ app/assets/javascripts/my/memos.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
 
   $(".new-memo-button").on("click", ".show-new-memo-button", function(){
     $(this).toggleClass("show-new-memo-button hide-new-memo-button").text("Hide New Memo");
-    $("html, body").animate({ scrollTop: $(document).height() });
+    $("html, body").animate({ scrollTop: $(document).height() - 100 + ($(".nested-fields").length * 399) });
   });
 
   $(".new-memo-button").on("click", ".hide-new-memo-button", function(){
     $(this).toggleClass("show-new-memo-button hide-new-memo-button").text("New Memo");
+  });
+
+  $(".segments").on("click", ".add-new-segment", function() {
+    $("html, body").animate({ scrollTop: $(document).height() - 259 });
   });
 
 });
