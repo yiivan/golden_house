ebb4e6c514bc084aabd68ca53c62fd4652dd7a80
yiivan <yiivan@hotmail.com>
2016-07-10 21:49:13 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,49 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# User.destroy_all
-# Category.destroy_all
-# Language.destroy_all
+User.destroy_all
+Category.destroy_all
+Language.destroy_all
 
-# User.create([{username: "yiivan", first_name: "Yii Van", last_name: "Tay",
-#               email: "yiivan@gmail.com", password: "abc",
-#               password_confirmation: "abc", admin: true},
-#              {username: "evan", first_name: "Evan", last_name: "Tay",
-#               email: "evan@gmail.com", password: "abc",
-#               password_confirmation: "abc"},
-User.create(username: "guest", first_name: "Guest", last_name: "Tay",
+User.create([{username: "yiivan", first_name: "Yii Van", last_name: "Tay",
+              email: "yiivan@gmail.com", password: "abc",
+              password_confirmation: "abc", admin: true},
+             {username: "evan", first_name: "Evan", last_name: "Tay",
+              email: "evan@gmail.com", password: "abc",
+              password_confirmation: "abc"},
+             {username: "guest", first_name: "Guest", last_name: "Tay",
               email: "guest@gmail.com", password: "abc",
-              password_confirmation: "abc")
+              password_confirmation: "abc"}])
 
-# Category.create([{name: "Arts"},
-#                  {name: "Biography & True Stories"},
-#                  {name: "Language"},
-#                  {name: "Literature & Literary Studies"},
-#                  {name: "English Language Teaching"},
-#                  {name: "Fiction & Related Items"},
-#                  {name: "Reference, Information & Interdisciplinary Studies"},
-#                  {name: "Humanities"},
-#                  {name: "Society & Social Sciences"},
-#                  {name: "Economics, Finance, Business & Management"},
-#                  {name: "Law"},
-#                  {name: "Medicine"},
-#                  {name: "Mathematics & Science"},
-#                  {name: "Earth Sciences, Geography, Environment, Planning"},
-#                  {name: "Technology, Engineering, Agriculture"},
-#                  {name: "Computing & Information Technology"},
-#                  {name: "Health & Personal Development"},
-#                  {name: "Lifestyle, Sport & Leisure"},
-#                  {name: "Children’s, Teenage & Educational"},
-#                  {name: "Other"}])
-#
-# Language.create([{name: "Chinese"},
-#                  {name: "Dutch"},
-#                  {name: "English"},
-#                  {name: "French"},
-#                  {name: "German"},
-#                  {name: "Italian"},
-#                  {name: "Japanese"},
-#                  {name: "Korean"},
-#                  {name: "Portuguese"},
-#                  {name: "Spanish"},
-#                  {name: "Other"}])
+Category.create([{name: "Arts"},
+                 {name: "Biography & True Stories"},
+                 {name: "Language"},
+                 {name: "Literature & Literary Studies"},
+                 {name: "English Language Teaching"},
+                 {name: "Fiction & Related Items"},
+                 {name: "Reference, Information & Interdisciplinary Studies"},
+                 {name: "Humanities"},
+                 {name: "Society & Social Sciences"},
+                 {name: "Economics, Finance, Business & Management"},
+                 {name: "Law"},
+                 {name: "Medicine"},
+                 {name: "Mathematics & Science"},
+                 {name: "Earth Sciences, Geography, Environment, Planning"},
+                 {name: "Technology, Engineering, Agriculture"},
+                 {name: "Computing & Information Technology"},
+                 {name: "Health & Personal Development"},
+                 {name: "Lifestyle, Sport & Leisure"},
+                 {name: "Children’s, Teenage & Educational"},
+                 {name: "Other"}])
+
+Language.create([{name: "Chinese"},
+                 {name: "Dutch"},
+                 {name: "English"},
+                 {name: "French"},
+                 {name: "German"},
+                 {name: "Italian"},
+                 {name: "Japanese"},
+                 {name: "Korean"},
+                 {name: "Portuguese"},
+                 {name: "Spanish"},
+                 {name: "Other"}])
