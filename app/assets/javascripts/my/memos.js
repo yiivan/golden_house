6b308f8ab1fa794a720bc2fbb8ae606e574833b8
yiivan <yiivan@hotmail.com>
2016-07-11 17:13:02 -0700

    customize text editor

--- app/assets/javascripts/my/memos.js
+++ app/assets/javascripts/my/memos.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $(".new-memo-button").on("click", ".show-new-memo-button", function(){
     $(this).toggleClass("show-new-memo-button hide-new-memo-button").text("Hide New Memo");
     $("html, body").animate({ scrollTop: $(document).height() - 24 });
-    CKEDITOR.replace( "memo-editor-new_memo" );
+    CKEDITOR.replace( "memo-editor-new_memo", {toolbar : 'special'});
   });
 
   $(".new-memo-button").on("click", ".hide-new-memo-button", function(){
@@ -16,5 +16,4 @@ $(document).ready(function() {
     $("#show-" + memoId).show();
   });
 
-  CKEDITOR.replace( "memo-editor-new_memo" );
 });
