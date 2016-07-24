e11513694d8a6c06ef11353022d7cdbbd7127f4e
yiivan <yiivan@hotmail.com>
2016-07-24 16:32:55 -0700

    redirect to previous page after signed in

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -4,12 +4,16 @@ class GuestSessionsController < ApplicationController
   def create
     user = User.find_by_email "guest@gmail.com"
     sign_in(user)
-    redirect_to root_path, notice: "Signed in as a guest!"
+    redirect_to session[:my_previous_url], notice: "Signed in as a guest!"
+    session[:my_previous_url] = ""
   end
 
   private
 
   def redirect_if_signed_in
-    redirect_to root_path, notice: "Already signed in as #{current_user.first_name}!" if user_signed_in?
+    if user_signed_in?
+      redirect_to session[:my_previous_url], notice: "Already signed in as #{current_user.first_name}!"
+      session[:my_previous_url] = ""
+    end
   end
 end
